fbe18d1695783e3696d5f774af2e219cfa03b696
Nihal Nihalani <nihal.nihalani@gmail.com>
2020-10-05 03:34:06 +0530

    project 4 beginning

---
+++ Project-4/code/main.py
@@ -0,0 +1,18 @@
+import numpy
+from numpy import *
+import pandas as pd
+import csv
+import hashlib
+import re
+from sklearn import *
+import random
+import numpy as np
+import os
+from math import sqrt
+import math
+import time
+from sklearn.utils.testing import ignore_warnings
+from sklearn.exceptions import ConvergenceWarning
+from warnings import simplefilter
+from sklearn.exceptions import ConvergenceWarning
+from sklearn import linear_model, svm, neural_network
